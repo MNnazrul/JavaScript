e1998eaf00d1ceda15fce325c6c96a0125626680
MNnazrul <mnnazrulislam196130@gmail.com>
2023-04-03 15:55:51 +0600

    all

--- JS/common/index.js
+++ JS/common/index.js
@@ -348,6 +348,38 @@ console.log(...class1);
 
 //----------------------------------------------------------------
 
+//48th
+
+// nested functions = Functions inside other functions.
+//  Outer functions have access to inner functions.
+//  Inner functions are "hidden" from outside.
+//   used in closures (future video topic)
+ 
+
+// let userName = "bro";
+// let userInbox = 0;
+// login();
+
+// function login() {
+//    displayUserName();
+//    displayUserInbox();
+
+//    function displayUserName() {
+//       console.log(`Welcome ${userName}`);
+//    }
+//    function displayUserInbox() {
+//       console.log(`You have ${userInbox} new messages.`);
+//    }
+// }
+
+//----------------------------------------------------
+
+
+
+
+
+
+
 
 
 
