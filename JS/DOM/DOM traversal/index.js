95e1e9495fd2d54b3313a136df9fa27c4c31c4e4
MNnazrul <mnnazrulislam196130@gmail.com>
2023-04-16 12:33:43 +0600

    add

---
+++ JS/DOM/DOM traversal/index.js
@@ -0,0 +1,28 @@
+
+
+// uses of firstElementChild.
+// let element = document.body;
+// let child = element.firstElementChild;
+// child.style.backgroundColor = "lightgreen";
+// console.log(child);
+
+// uses of parentElement
+let ele = document.querySelector("#vegetables");
+// let parent = ele.parentElement;
+// parent.style.backgroudColor = "lightgreen"
+// // console.log(parent);
+// let sibling = ele.nextElementSibling;
+// sibling.style.color = "lightgreen";
+// console.log(sibling);
+let ch = ele.firstElementChild;
+ch.style.color = "lightgreen"
+
+
+
+// .firstElementChild
+// .lastElementChild
+// .parentElement
+// .nextElementSibling
+// .previousElementSibling
+// .children[]
+// Array.from(.children)
