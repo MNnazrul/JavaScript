97e64c208ca0bfa77c9aaea2d1e32d571337ddc6
MNnazrul <mnnazrulislam196130@gmail.com>
2023-04-16 13:07:10 +0600

    add

---
+++ JS/DOM/add or change css properties/index.js
@@ -0,0 +1,7 @@
+const title = document.getElementById("myTitle");
+title.style.backgroundColor = "rgb(50, 200, 250)";
+title.style.color = "red";
+title.style.fontFamily = "consolas";
+title.style.textAlign = "center";
+title.style.border = "2px solid";
+title.style.display = "block";
