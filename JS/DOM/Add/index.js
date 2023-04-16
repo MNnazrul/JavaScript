ecdec7df7e1335a753059a577984399cfe124f6a
MNnazrul <mnnazrulislam196130@gmail.com>
2023-04-16 12:32:28 +0600

    add

---
+++ JS/DOM/Add/index.js
@@ -0,0 +1,14 @@
+// add/change HTML elements
+// .innerHTML(vulnerable to XSS attacks)
+// .textContent
+
+// const nameTag = document.createElement("h1");
+// nameTag.textContent = window.prompt("Enter your name : ");
+// document.body.append(nameTag);
+
+const myList = document.querySelector("#fruits");
+const listItem = document.createElement("li");
+listItem.textContent = "Mango";
+// myList.append(listItem);
+// myList.prepend(listItem);
+myList.insertBefore(listItem, myList.getElementsByTagName("li")[2]);
